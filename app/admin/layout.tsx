8e1007a3f62a8e250a8e9f7a594a93510ad59f5f
huangtao <1758042125@qq.com>
2025-07-28 00:24:09 +0800

    【Refactor】优化Admin 端布局

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,4 @@
-import AdminSideBar from '@/components/admin-side-bar'
 import AdminLayout from '@/components/admin/layout'
-import BreadCrumb from '@/components/breadcrumb'
-import ThemeSwitch from '@/components/liaght-dark-mode-switch'
 import SessionProvider from '@/components/provider/session-provider'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
@@ -15,20 +12,6 @@ export default async function Layout({ children }: PropsWithChildren) {
   }
   return (
     <SessionProvider session={session}>
-      {/* <div className="flex h-screen">
-        <div>
-          <AdminSideBar />
-        </div>
-        <div className="h-screen flex flex-col p-8 pb-0 w-[100%]">
-          <div className="h-[60px] flex items-center border-b justify-between">
-            <BreadCrumb />
-            <ThemeSwitch />
-          </div>
-          <main className="overflow-scroll h-[calc(100vh-60px)] mt-4 pr-2">
-            {children}
-          </main>
-        </div>
-      </div> */}
       <AdminLayout>{children}</AdminLayout>
     </SessionProvider>
   )
