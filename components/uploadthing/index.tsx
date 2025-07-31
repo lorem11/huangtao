af28a62b3da524b74f3a053d3039164320012977
huangtao <1758042125@qq.com>
2025-07-31 23:08:24 +0800

    【New】upload button 自定义样式

--- components/uploadthing/index.tsx
+++ components/uploadthing/index.tsx
@@ -3,5 +3,14 @@ import {
   generateUploadDropzone,
 } from '@uploadthing/react'
 import type { OurFileRouter } from '@/app/api/uploadthing/core'
-export const UploadButton = generateUploadButton<OurFileRouter>()
+import { ComponentProps } from 'react'
+const Button = generateUploadButton<OurFileRouter>()
+export const UploadButton = ({
+  ...props
+}: Omit<ComponentProps<typeof Button>, 'className'>) => (
+  <Button
+    className="ut-button:h-9 ut-button:bg-primary ut-button:text-primary-foreground"
+    {...props}
+  />
+)
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
