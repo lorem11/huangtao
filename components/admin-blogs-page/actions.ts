1d731193f75941e67b15e4b24b68c72f4b4a8c36
huangtao <1758042125@qq.com>
2025-08-03 16:02:07 +0800

    【New】创建文章新增描述字端

--- components/admin-blogs-page/actions.ts
+++ components/admin-blogs-page/actions.ts
@@ -3,14 +3,36 @@
 import prisma from '@/lib/prisma'
 import { CreateBlogForm } from './types'
 
+export async function getAllBlogItem() {
+  return await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+    select: {
+      title: true,
+      slug: true,
+      cover: true,
+      desc: true,
+      tags: {
+        select: {
+          name: true,
+          icon: true,
+          iconDark: true,
+        },
+      },
+      createdAt: true,
+    },
+  })
+}
+
 export async function createBlog(params: CreateBlogForm) {
   const { tags, ...data } = params
+  console.log(tags)
   await prisma.post.create({
     data: {
       ...data,
       tags: { connect: tags!.map((id) => ({ id })) },
     },
   })
-  console.log(params)
   return '提交成功'
 }
